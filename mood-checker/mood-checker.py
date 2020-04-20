f38dc0f926777f3af9bc7e28a2bac077d1e4ed3e
Martin Michel Majcenovič <60785701+MartinMichel1@users.noreply.github.com>
2020-04-20 19:23:55 +0200

    Create mood-checker.py

---
+++ mood-checker/mood-checker.py
@@ -0,0 +1,13 @@
+mood_input = input("How are you feeling at the moment?")
+if mood_input == "happy":
+    print("It is great to see you happy!")
+elif mood_input == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood_input == "sad":
+    print("Stop being sad and be awesome instead")
+elif mood_input == "excited":
+    print("That's good news!")
+elif mood_input == "relaxed":
+    print("That's good to hear")
+else:
+    print("I don't recognize this mood")
